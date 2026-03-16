3c17607e9affd46faab05525d7767fd797557e91
MathiewMay <mathiewmaysenhoelder@hotmail.com>
2026-03-16 12:38:55 -0400

    Added gamemode property to ServerPlayer and setGameMode and getGamemode, fixed typo in the lua PlayerBlockPlaceEvent name

--- Common/EventSystem/EventBindings.cpp
+++ Common/EventSystem/EventBindings.cpp
@@ -2,6 +2,7 @@
 
 #include "../Minecraft.Client/Player/ServerPlayerGameMode.h"
 #include "../Minecraft.Client/Player/ServerPlayer.h"
+#include "../Minecraft.World/Level/Storage/LevelSettings.h"
 
 /* Server Includes */
 #include "Server/Events/Item/ItemInteractEvent.h"
@@ -30,13 +31,24 @@ void EventBindings::bindServerEvents(sol::state& lua) {
         }
     );
 
+    lua.new_usertype<ServerPlayerGameMode>("ServerPlayerGameMode",
+        "isSurvival", sol::property(&ServerPlayerGameMode::isSurvival),
+        "isCreative", sol::property(&ServerPlayerGameMode::isCreative)
+    );
+
+    lua["GameMode"] = sol::table(lua, sol::create);
+    lua["GameMode"]["NOT_SET"]   = 0;
+    lua["GameMode"]["SURVIVAL"]  = 0;
+    lua["GameMode"]["CREATIVE"]  = 1;
+    lua["GameMode"]["ADVENTURE"] = 2;
+
     lua.new_usertype<ServerPlayer>("ServerPlayer",
-        "getGamemode", sol::property([](ServerPlayer& p) -> std::string {
-            if (p.gameMode->isSurvival()) return "survival";
-            if (p.gameMode->isCreative()) return "creative";
-            return "unknown";
-        }),
+        "gamemode", &ServerPlayer::gameMode,
         "inventory", &ServerPlayer::inventory,
+        "setGameMode", [](ServerPlayer& player, int gameTypeId) {
+            GameType* type = GameType::byId(gameTypeId);
+            if (type) player.setGameMode(type);
+        },
         "sendMessage", [](ServerPlayer& p, const std::string& message) {
             p.sendMessage(std::wstring(message.begin(), message.end()));
         }
@@ -51,7 +63,7 @@ void EventBindings::bindServerEvents(sol::state& lua) {
         sol::base_classes, sol::bases<CancellableCactusEvent, CactusEvent>()
     );
 
-    lua.new_usertype<PlayerBlockPlaceEvent>("PlayerPlaceBlockEvent",
+    lua.new_usertype<PlayerBlockPlaceEvent>("PlayerBlockPlaceEvent",
         "player", &PlayerBlockPlaceEvent::player,
         "x", &PlayerBlockPlaceEvent::x,
         "y", &PlayerBlockPlaceEvent::y,
--- ExampleMod/module/server.lua
+++ ExampleMod/module/server.lua
@@ -6,17 +6,16 @@ function main()
 	log((number+2).."<- this is number 10")
 
 	registerEvent("PlayerBlockBreak", function(event)
-		if event.player.gamemode ~= "creative" then
-			event.player.inventory:setItem(0, 407)
-			event.player.inventory:setItem(1, 408)
+		if event.player.gamemode.isSurvival then
+            event.player:setGameMode(GameMode.CREATIVE)
 			event.player:sendMessage("You are not allowed to break block id "..event.blockId)
 			event:setCancelled(true)
 		end
 	end)
 
 	registerEvent("PlayerBlockPlace", function(event)
-		if event.player.gamemode ~= "creative" then
-			event.player:sendMessage("You are not allowed to place block id "..event.blockId.." located")
+		if event.player.gamemode.isSurvival then
+			event.player:sendMessage("You are not allowed to place block id "..event.blockId)
 			event:setCancelled(true)
 		end
 	end)
